cf324500e34de30c8672f699211f571549e1f7b6
touraye <touraye07@gmail>
2024-04-30 16:28:30 +0000

    changed permission to "isAuthenticated"

--- hosital_m/hospital/api.py
+++ hosital_m/hospital/api.py
@@ -4,26 +4,38 @@ from .serializers import DoctorSerializer, PatientSerializer, AppointmentSeriali
 
 # Doctor Viewset
 class DoctorViewSet(viewsets.ModelViewSet):
-    queryset = Doctor.objects.all()
+     # queryset = Doctor.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated # adding authentication
     ]
+    
     serializer_class = DoctorSerializer
+    
+    def get_queryset(self):        
+        return Doctor.objects.all()
     
     
 # Patient Viewset
 class PatientViewSet(viewsets.ModelViewSet):
-    queryset = Patient.objects.all()
+    # queryset = Patient.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated # adding authentication
     ]
+    
     serializer_class = PatientSerializer  
+    
+    def get_queryset(self):
+        return self.Patient.objects.all()
     
     
 # Appointment Viewset
 class AppointmentViewSet(viewsets.ModelViewSet):
-    queryset = Appointment.objects.all()
+    # queryset = Appointment.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated # add authentication
     ]
-    serializer_class = AppointmentSerializer      
+    
+    serializer_class = AppointmentSerializer   
+    
+    def get_queryset(self):
+        return self.Appointment.objects.all()
